f49bda3f333cc87463aed6d50f3714f8e9aabcce
dsoto06 <diegosr06@hotmail.com>
2021-02-25 03:07:53 +0000

    entrega cards

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,8 +2,19 @@ import React from "react";
 
 //include images into your bundle
 import { NavBar } from "./navbar.js";
+import { Cards } from "./cards.js";
 
 //create your first component
 export function Home() {
-	return <NavBar />;
+	return (
+		<div>
+			<NavBar />
+			<div className="row">
+				<Cards />
+				<Cards />
+				<Cards />
+				<Cards />
+			</div>
+		</div>
+	);
 }
