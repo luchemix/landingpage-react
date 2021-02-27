f2eb52ba5ba3acb45815944141ef9a14bd17ba6b
luchemix <luchemix@gmail.com>
2021-02-27 01:25:10 +0000

    final nav

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,83 +6,52 @@ export function NavBar() {
 		<div>
 			<nav className="navbar navbar-expand-lg navbar-light bg-dark">
 				<div className="container-fluid">
-					<a className="navbar-brand text-light" href="#">
-						<img src="4geeks.ico"></img>
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div
-						className="collapse navbar-collapse"
-						id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<a
-									className="nav-link active text-light"
-									aria-current="page"
-									href="#">
-									Home
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link text-light" href="#">
-									Link
-								</a>
-							</li>
-							<li className="nav-item dropdown">
-								<a
-									className="nav-link dropdown-toggle text-light"
-									href="#"
-									id="navbarDropdown"
-									role="button"
-									data-bs-toggle="dropdown"
-									aria-expanded="false">
-									Dropdown
-								</a>
-								<ul
-									className="dropdown-menu"
-									aria-labelledby="navbarDropdown">
-									<li>
-										<a className="dropdown-item" href="#">
-											Action
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="#">
-											Another action
-										</a>
-									</li>
-									<li>
-										<hr className="dropdown-divider" />
-									</li>
-									<li>
-										<a className="dropdown-item" href="#">
-											Something else here
-										</a>
-									</li>
-								</ul>
-							</li>
-						</ul>
-						<form className="d-flex justify-content-end text-light">
-							<input
-								className="form-control me-2 d-flex justify-content-end"
-								type="search"
-								placeholder="Search"
-								aria-label="Search"
-							/>
-							<button
-								className="btn btn-outline-success d-flex justify-content-end text-light"
-								type="submit">
-								Search
-							</button>
-						</form>
+					<div className="col-3">
+						<a className="navbar-brand text-light" href="#">
+							<img src="4geeks.ico"></img>
+						</a>
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarSupportedContent"
+							aria-controls="navbarSupportedContent"
+							aria-expanded="false"
+							aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon"></span>
+						</button>
+					</div>
+					<div className="col-5"></div>
+					<div className="col-3">
+						<div
+							className="collapse navbar-collapse"
+							id="navbarSupportedContent">
+							<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+								<li className="nav-item">
+									<a
+										className="nav-link active text-light"
+										aria-current="page"
+										href="#">
+										Home
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-light" href="#">
+										About
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-light" href="#">
+										Services
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-light" href="#">
+										Contact
+									</a>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</nav>
